bec3c1c795008b7eb03f7fd80d541d19aebbee19
bonginn <love5274152741@gmail.com>
2024-08-07 02:10:48 +0800

    add Basic

---
+++ Basic/default.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define IOS cin.tie(0);ios_base::sync_with_stdio(false);
+#define INF 1e15+9
+using namespace std;
+const int maxn = 2e5+5;
+const int MOD = 1e9+7; // 998244353;
+typedef pair<int,int> P;
+void solve(){
+    
+}
+
+signed main(){IOS
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+}
