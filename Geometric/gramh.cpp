258e1e02d19deeebb9df18af55223709b529c1b2
Cheng0303 <62131278+Cheng0303@users.noreply.github.com>
2024-08-10 22:01:17 +0800

    Update gramh.cpp

--- Geometric/gramh.cpp
+++ Geometric/gramh.cpp
@@ -1,15 +1,11 @@
-
 //記得先定義叉積與減
 vector<pdd> convexHull(vector<pdd>& ps) {
-
     sort(all(ps));
     vector<pdd> hull;
     if (ps.size() <= 2) {
         return ps;
     }
-
     for (int i = 0; i < 2; i++) {
-
         int s = hull.size();
         for (pdd p : ps) {
             while (hull.size() - s >= 2 && cross(hull.back() - hull[hull.size() - 2], p - hull[hull.size() - 2]) < 1e-10) {
@@ -20,6 +16,5 @@ vector<pdd> convexHull(vector<pdd>& ps) {
         hull.ppb();
         reverse(all(ps));
     }
-
     return hull;
 }
