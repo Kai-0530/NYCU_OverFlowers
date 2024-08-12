05e89d00053c36bcb79382ddc688d244d3b304e3
bonginn <love5274152741@gmail.com>
2024-08-13 01:11:43 +0800

    update Default.cpp

--- Basic/default.cpp
+++ Basic/default.cpp
@@ -2,17 +2,18 @@
 #define int long long
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
-#define IOS cin.tie(0);ios_base::sync_with_stdio(false);
+#define fastio cin.tie(0); ios_base::sync_with_stdio(false);
 #define INF 1e15+9
 using namespace std;
-const int maxn = 2e5+5;
+const int maxn = 2e5+5; 
 const int MOD = 1e9+7; // 998244353;
 typedef pair<int,int> P;
+
 void solve(){
-    
+    // Do Something...
 }
 
-signed main(){IOS
+signed main(){fastio
     int T = 1;
     cin >> T;
     while(T--){
