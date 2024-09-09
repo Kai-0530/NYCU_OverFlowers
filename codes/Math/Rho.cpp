5b7cbe02ee4f9942105d66f1763faa73185a4dcd
Kai-0530 <86472793+Kai-0530@users.noreply.github.com>
2024-09-09 15:55:42 +0800

    Update Rho.cpp

--- codes/Math/Rho.cpp
+++ codes/Math/Rho.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-ll max_factor, n;
-ll gcd(ll a, ll b) {
+__int128 n;
+__int128 gcd(__int128 a, __int128 b) {
   if (b == 0) return a;
   return gcd(b, a % b);
 }
 
-// read, print, cmp
 __int128 read() {
     __int128 x = 0, f = 1;
     char ch = getchar();
@@ -22,6 +20,7 @@ __int128 read() {
     }
     return x * f;
 }
+
 void print(__int128 x) {
     if (x < 0) {
         putchar('-');
@@ -32,9 +31,8 @@ void print(__int128 x) {
 }
 bool cmp(__int128 x, __int128 y) { return x > y; }
 
-
-ll quick_pow(ll x, ll p, ll mod) { 
-  ll ans = 1;
+__int128 quick_pow(__int128 x, __int128 p, __int128 mod) { 
+  __int128 ans = 1;
   while (p) {
     if (p & 1) ans = (__int128)ans * x % mod;
     x = (__int128)x * x % mod;
@@ -43,15 +41,15 @@ ll quick_pow(ll x, ll p, ll mod) {
   return ans;
 }
 
-bool Miller_Rabin(ll p) {
+bool Miller_Rabin(__int128 p) {
   if (p < 2) return 0;
   if (p == 2) return 1;
   if (p == 3) return 1;
-  ll d = p - 1, r = 0;
+  __int128 d = p - 1, r = 0;
   while (!(d & 1)) ++r, d >>= 1; 
-  for (ll k = 0; k < 10; ++k) {
-    ll a = rand() % (p - 2) + 2;
-    ll x = quick_pow(a, d, p);
+  for (int k = 0; k < 10; ++k) {
+    __int128 a = (__int128)rand() % (p - 2) + 2;
+    __int128 x = quick_pow(a, d, p);
     if (x == 1 || x == p - 1) continue;
     for (int i = 0; i < r - 1; ++i) {
       x = (__int128)x * x % p;
@@ -62,51 +60,64 @@ bool Miller_Rabin(ll p) {
   return 1;
 }
 
-ll Pollard_Rho(ll x) {
-  ll s = 0, t = 0;
-  ll c = (ll)rand() % (x - 1) + 1;
-  int step = 0, goal = 1;
-  ll val = 1;
+__int128 Pollard_Rho(__int128 x) {
+  __int128 s = 0, t = 0;
+  __int128 c = (__int128)rand() % (x - 1) + 1;
+  __int128 step = 0, goal = 1;
+  __int128 val = 1;
   for (goal = 1;; goal *= 2, s = t, val = 1) { 
     for (step = 1; step <= goal; ++step) {
       t = ((__int128)t * t + c) % x;
-      val = (__int128)val * abs(t - s) % x;
+      __int128 ts;
+      if(t >= s) ts = t-s;
+      else ts = s-t;
+      val = (__int128)val * ts % x;
       if ((step % 127) == 0) {
-        ll d = gcd(val, x);
+        __int128 d = gcd(val, x);
         if (d > 1) return d;
       }
     }
-    ll d = gcd(val, x);
+    __int128 d = gcd(val, x);
     if (d > 1) return d;
   }
 }
 
-void fac(ll x) {
-  if (x <= max_factor || x < 2) return;
-  if (Miller_Rabin(x)) {            
-    max_factor = max(max_factor, x); 
-    return;
-  }
-  ll p = x;
-  while (p >= x) p = Pollard_Rho(x);  
-  while ((x % p) == 0) x /= p;
-  fac(x), fac(p);  // 向下分解x和p
+set<__int128> st;
+void fac(__int128 x){
+    if(x<2) return;
+    if(Miller_Rabin(x)){
+        st.insert(x);
+        return;
+    }
+    __int128 p = x;
+    while(p>=x) p = Pollard_Rho(x);
+    while((x%p) == 0) x /= p;
+    fac(p); fac(x);
+}
+
+vector<__int128> v; // prime factors
+set<__int128> allf; // factors
+void dfs(__int128 res, __int128 now){
+    auto it = allf.find(now);
+    if(it != allf.end()) return;
+    allf.insert(now);
+    if(res < 2) return;
+    for(__int128 i:v){
+        if(res % i == 0){
+            dfs(res / i, now * i);
+        }
+    }
 }
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    int t;
-    cin>>t;
-    while(t--){
-        srand((unsigned)time(NULL));
-        max_factor = 0;
-        cin>>n;
-        fac(n);
-        if(max_factor==n){
-            cout<<"Prime\n";
-        }
-        else{
-            cout<<max_factor<<"\n";
-        }
+    srand((unsigned)time(NULL));
+    n = read();
+    __int128 N = (__int128)(n) * n;
+    fac(n); // find all n's prime factor
+    for(__int128 i:st) v.push_back(i);
+    dfs(N, 1);
+    // allf = all N's factors
+    for(auto i:allf){
+        print(i); cout<<endl;
     }
 }
